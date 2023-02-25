a97803bb24f9e1ff9818478b53c6e92e86b63dca
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-25 15:36:52 +0100

    Use mongoose virturals to delete _id and __v fiels in the DB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import "./loadEnvironment.js";
+import mongoose from "mongoose";
 import connectDatabase from "./database/connectDatabase.js";
 import startServer from "./server/startServer.js";
 
 const port = process.env.PORT ?? 4000;
 const databaseUrl = process.env.MONGODB_CONNECTION_URL!;
+
+mongoose.set("toJSON", {
+  virtuals: true,
+  transform(doc, ret) {
+    delete ret._id;
+    delete ret.__v;
+  },
+});
 
 await connectDatabase(databaseUrl);
 await startServer(+port);
