86cbd30c9cdd276000e9b6472854d21a00ae625e
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-25 12:48:05 +0100

    Install multer and add post method to usersRouters

--- src/server/routers/usersRouters.ts
+++ src/server/routers/usersRouters.ts
@@ -1,6 +1,22 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/usersControllers.js";
+import multer from "multer";
+import { createUser, getUsers } from "../controllers/usersControllers.js";
 
 export const usersRouters = Router();
 
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "uploads/");
+  },
+  filename(req, file, callback) {
+    callback(
+      null,
+      `${Date.now()}-${(Math.random() * 1000).toFixed(0)}-${file.originalname}`
+    );
+  },
+});
+
+const upload = multer({ storage });
+
 usersRouters.get("/users", getUsers);
+usersRouters.post("/users", upload.single("avatar"), createUser);
